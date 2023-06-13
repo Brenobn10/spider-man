77c9fc6acc0f813445d76ccec81ede3f3d0c2ccf
Brenobn10 <136459752+Brenobn10@users.noreply.github.com>
2023-06-13 11:04:55 -0300

    pedindo para sair

---
+++ script.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert("Você aceitou assistir o filminho cmg! <3")
+}
+
+function desvia(t) {
+    var btn = t;
+    btn.style.position = "absolute";
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log("opa, desviei...");
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
